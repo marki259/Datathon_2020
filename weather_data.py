138c07fe7865a5d06575c9071e4914fec6b66a8b
marc-andre <marcandrechenier@gmail.com>
2020-02-11 22:57:54 +0100

    Mapped weather sensor data

--- weather_data.py
+++ weather_data.py
@@ -7,6 +7,7 @@ local_path = '/home/marc/School/datathon_data'
 
 os.listdir(local_path)
 
+## Checking the air data
 la19_dat = pd.read_csv(local_path + '/leuvenair_2019.csv')
 la18_dat = pd.read_csv(local_path + '/leuvenair_2018.csv')
 la20_dat = pd.read_csv(local_path + '/leuvenair_2020.csv')
@@ -34,6 +35,24 @@ import mplleaflet
 
 plt.plot(map_dat['LON'], map_dat['LAT'], 'p')
 mplleaflet.show()
-        
+
+## Checking the weather data 
+garmon_dat = pd.read_csv(local_path + '/garmon_0203.csv')
+
+## About 7 months of data up to February. 
+garmon_dat['DATEUTC'] = garmon_dat['DATEUTC'].astype('datetime64')
+garmon_dat['DATEUTC'].describe()
+
+sg_dat = garmon_dat[['ID', 'LAT', 'LON']]
+
+sg_dat = sg_dat.groupby('ID').agg({'LAT':'first', 'LON':'first'})
+sg_dat = sg_dat.reset_index()
+
+## Write weather sensor coordinates data and map it
+sg_dat.to_csv('weatherSensor_coordinates.csv')
+
+## Similar dispersion as with air data (a bit less sensors)
+plt.plot(sg_dat['LON'], sg_dat['LAT'], 'p')
+mplleaflet.show()
 
 
